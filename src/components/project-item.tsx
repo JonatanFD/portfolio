15b41253c9d4a99d05da2f9b783103e0805d50db
JonatanFD <u20211b995@upc.edu.pe>
2026-04-05 12:13:31 -0700

    feat: add cover url and buttons to link another source code and website

--- src/components/project-item.tsx
+++ src/components/project-item.tsx
@@ -1,4 +1,6 @@
+import { GlobeIcon } from "@phosphor-icons/react";
 import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 import {
     Card,
     CardContent,
@@ -6,6 +8,8 @@ import {
     CardHeader,
     CardTitle,
 } from "./ui/card";
+import { GithubDark } from "./ui/svgs/githubDark";
+import { GithubLight } from "./ui/svgs/githubLight";
 
 interface Props {
     title: string;
@@ -19,16 +23,51 @@ export default function ProjectItem({
     title,
     description,
     coverUrl,
+    githubUrl,
+    websiteUrl,
     tags,
 }: Props) {
     return (
-        <Card>
+        <Card className="h-full gap-10 justify-between">
             <CardHeader>
+                <div className="mb-4 space-y-1">
+                    <img
+                        src={coverUrl}
+                        alt={title}
+                        className="rounded-xl border"
+                    />
+
+                    <div className="space-x-1">
+                        <a href={githubUrl} target="_blank">
+                            <Button
+                                size="sm"
+                                variant="outline"
+                                className="gap-2 items-center"
+                            >
+                                <GithubDark className="hidden dark:block" />
+                                <GithubLight className="block dark:hidden" />
+                                <span>Source</span>
+                            </Button>
+                        </a>
+                        {websiteUrl !== "" && (
+                            <a href={websiteUrl} target="_blank">
+                                <Button
+                                    size="sm"
+                                    variant="outline"
+                                    className="gap-2 items-center"
+                                >
+                                    <GlobeIcon />
+                                    <span>Website</span>
+                                </Button>
+                            </a>
+                        )}
+                    </div>
+                </div>
                 <CardTitle>{title}</CardTitle>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
             <CardContent>
-                <ul className="space-x-2">
+                <ul className="flex flex-wrap gap-1 mt-auto">
                     {tags.map((tag) => (
                         <li key={tag} className="inline">
                             <Badge>{tag}</Badge>
